c182be55d87e2455aa16fb0564371827d49e3a2d
atash2012 <x.shojaei@gmail.com>
2017-07-22 11:37:35 -0600

    Add files via upload

---
+++ example.lua
@@ -0,0 +1,35 @@
+--[[
+    This example will echo messages back to the user who sent them, with an inline keyboard
+    which tells the user the JSON for the callback_query.from object.
+]]
+
+local api = require('telegram-bot-lua.core').configure('') -- Enter your token
+local json = require('dkjson')
+
+function api.on_message(message)
+    if message.text then
+        api.send_message(
+            message,
+            message.text,
+            nil,
+            true,
+            false,
+            nil,
+            api.inline_keyboard():row(
+                api.row():callback_data_button(
+                    'Button',
+                    'callback_data'
+                )
+            )
+        )
+    end
+end
+
+function api.on_callback_query(callback_query)
+    api.answer_callback_query(
+        callback_query.id,
+        json.encode(callback_query.from)
+    )
+end
+
+api.run()
